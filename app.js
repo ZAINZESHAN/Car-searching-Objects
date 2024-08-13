9106719a3e0bba873bb363735bee936e45041571
ZAINZESHAN <153626862+ZAINZESHAN@users.noreply.github.com>
2024-08-13 20:59:00 +0500

    acceptable-dust.surge.sh

---
+++ app.js
@@ -0,0 +1,576 @@
+const car = {
+    Honda: {
+        Civic: {
+            carName: "Honda Civic",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.0L I4",
+            seatCapacity: "5",
+            fuelTankCapacity: "47 l",
+            image: "<img src='images/honda.civic.jpg' class='images'>"
+        },
+        Accord: {
+            carName: "Honda Accord",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.5L I4",
+            seatCapacity: "5",
+            fuelTankCapacity: "48 l",
+            image: "<img src='images/honda.Accord.jpg' class='images'>"
+        },
+        CRV: {
+            carName: "Honda CR-V",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.5L I4",
+            seatCapacity: "5",
+            fuelTankCapacity: "57 l",
+            image: "<img src='images/honda.crv.jpg' class='images'>"
+        },
+        HRV: {
+            carName: "Honda HR-V",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.0L I4",
+            seatCapacity: "5",
+            fuelTankCapacity: "50 l",
+            image: "<img src='images/honda.hrv.jpg' class='images'>"
+        },
+        Pilot: {
+            carName: "Honda Pilot",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "3.5L V6",
+            seatCapacity: "8",
+            fuelTankCapacity: "70 l",
+            image: "<img src='images/honda.pilot.jpg' class='images'>"
+        },
+        Ridgeline: {
+            carName: "Honda Ridgeline",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "3.5L V6",
+            seatCapacity: "5",
+            fuelTankCapacity: "65 l",
+            image: "<img src='images/honda.ridgeline.jpg' class='images'>"
+        },
+        Insight: {
+            carName: "Honda Insight",
+            model: "2023",
+            fuelType: "Hybrid",
+            engine: "1.5L I4",
+            seatCapacity: "5",
+            fuelTankCapacity: "40 l",
+            image: "<img src='images/honda.insight.jpg' class='images'>"
+        },
+        Odyssey: {
+            carName: "Honda Odyssey",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "3.5L V6",
+            seatCapacity: "7-8",
+            fuelTankCapacity: "50 l",
+            image: "<img src='images/honda.odyssey.jpg' class='images'>"
+        },
+        Passport: {
+            carName: "Honda Passport",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "3.5L V6",
+            seatCapacity: "5",
+            fuelTankCapacity: "70 l",
+            image: "<img src='images/honda.passport.jpg' class='images'>"
+        },
+        CivicTypeR: {
+            carName: "Honda Civic Type R",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.0L I4 Turbo",
+            seatCapacity: "5",
+            fuelTankCapacity: "47 l",
+            image: "<img src='images/honda.civictyper.jpg' class='images'>"
+        }
+    },
+    Toyota: {
+        Camry: {
+            carName: "Toyota Camry",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.5L I4",
+            seatCapacity: "5",
+            fuelTankCapacity: "50 l",
+            image: "<img src='images/toyota.camry.jpg' class='images'>"
+        },
+        Corolla: {
+            carName: "Toyota Corolla",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.0L I4",
+            seatCapacity: "5",
+            fuelTankCapacity: "50 l",
+            image: "<img src='images/toyota.corolla.jpg' class='images'>"
+        },
+        RAV4: {
+            carName: "Toyota RAV4",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.5L I4",
+            seatCapacity: "5",
+            fuelTankCapacity: "55 l",
+            image: "<img src='images/toyota.rav4.jpg' class='images'>"
+        },
+        Highlander: {
+            carName: "Toyota Highlander",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "3.5L V6",
+            seatCapacity: "7-8",
+            fuelTankCapacity: "65 l",
+            image: "<img src='images/toyota.highlander.jpg' class='images'>"
+        },
+        Tacoma: {
+            carName: "Toyota Tacoma",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "3.5L V6",
+            seatCapacity: "5",
+            fuelTankCapacity: "80 l",
+            image: "<img src='images/toyota.tacoma.jpg' class='images'>"
+        },
+        Tundra: {
+            carName: "Toyota Tundra",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "3.5L V6",
+            seatCapacity: "5-6",
+            fuelTankCapacity: "98 l",
+            image: "<img src='images/toyota.tundra.jpg' class='images'>"
+        },
+        Sienna: {
+            carName: "Toyota Sienna",
+            model: "2023",
+            fuelType: "Hybrid",
+            engine: "2.5L I4",
+            seatCapacity: "7-8",
+            fuelTankCapacity: "55 l",
+            image: "<img src='images/toyota.sienna.jpg' class='images'>"
+        },
+        Avalon: {
+            carName: "Toyota Avalon",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "3.5L V6",
+            seatCapacity: "5",
+            fuelTankCapacity: "65 l",
+            image: "<img src='images/toyota.avalon.jpg' class='images'>"
+        },
+        CHR: {
+            carName: "Toyota C-HR",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.0L I4",
+            seatCapacity: "5",
+            fuelTankCapacity: "43 l",
+            image: "<img src='images/toyota.chr.jpg' class='images'>"
+        },
+        GRSupra: {
+            carName: "Toyota GR Supra",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "3.0L I6 Turbo",
+            seatCapacity: "2",
+            fuelTankCapacity: "52 l",
+            image: "<img src='images/toyota.grsupra.jpg' class='images'>"
+        }
+    },
+    Suzuki: {
+        Jimny: {
+            carName: "Suzuki Jimny",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.5L",
+            seatCapacity: "4",
+            fuelTankCapacity: "40 l",
+            image: "<img src='images/Suzuki.jimny.jpg' class='images'>"
+        },
+        Swift: {
+            carName: "Suzuki Swift",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.2L I4",
+            seatCapacity: "5",
+            fuelTankCapacity: "37 l",
+            image: "<img src='images/Suzuki.swift.jpg' class='images'>"
+        },
+        Vitara: {
+            carName: "Suzuki Vitara",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.4L I4 Turbo",
+            seatCapacity: "5",
+            fuelTankCapacity: "47 l",
+            image: "<img src='images/Suzuki.vitara.jpg' class='images'>"
+        },
+        Ignis: {
+            carName: "Suzuki Ignis",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.2L I4",
+            seatCapacity: "4",
+            fuelTankCapacity: "27 l",
+            image: "<img src='images/Suzuki.ignis.jpg' class='images'>"
+        },
+        SCross: {
+            carName: "Suzuki S-Cross",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.4L I4 Turbo",
+            seatCapacity: "5",
+            fuelTankCapacity: "48 l",
+            image: "<img src='images/Suzuki.scross.jpg' class='images'>"
+        },
+        Alto: {
+            carName: "Suzuki Alto",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.0L I3",
+            seatCapacity: "4",
+            fuelTankCapacity: "27 l",
+            image: "<img src='images/Suzuki.alto.jpg' class='images'>"
+        },
+        Celerio: {
+            carName: "Suzuki Celerio",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.0L I3",
+            seatCapacity: "5",
+            fuelTankCapacity: "35 l",
+            image: "<img src='images/Suzuki.celerio.jpg' class='images'>"
+        },
+        Ertiga: {
+            carName: "Suzuki Ertiga",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.5L I4",
+            seatCapacity: "7",
+            fuelTankCapacity: "45 l",
+            image: "<img src='images/Suzuki.ertiga.jpg' class='images'>"
+        },
+        XL6: {
+            carName: "Suzuki XL6",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.5L I4",
+            seatCapacity: "6",
+            fuelTankCapacity: "45 l",
+            image: "<img src='images/Suzuki.xl6.jpg' class='images'>"
+        },
+        Baleno: {
+            carName: "Suzuki Baleno",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.2L I4",
+            seatCapacity: "5",
+            fuelTankCapacity: "37 l",
+            image: "<img src='images/Suzuki.baleno.jpg' class='images'>"
+        }
+    },
+    Audi: {
+        A3: {
+            carName: "Audi A3",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.0L I4",
+            seatCapacity: "5",
+            fuelTankCapacity: "50 l",
+            image: "<img src='images/audi.a3.jpg' class='images'>"
+        },
+        A4: {
+            carName: "Audi A4",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.0L I4",
+            seatCapacity: "5",
+            fuelTankCapacity: "54 l",
+            image: "<img src='images/audi.a4.jpg' class='images'>"
+        },
+        A6: {
+            carName: "Audi A6",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.0L I4",
+            seatCapacity: "5",
+            fuelTankCapacity: "70 l",
+            image: "<img src='images/audi.a6.jpg' class='images'>"
+        },
+        Q3: {
+            carName: "Audi Q3",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.0L I4",
+            seatCapacity: "5",
+            fuelTankCapacity: "64 l",
+            image: "<img src='images/audi.q3.jpg' class='images'>"
+        },
+        Q5: {
+            carName: "Audi Q5",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.0L I4",
+            seatCapacity: "5",
+            fuelTankCapacity: "70 l",
+            image: "<img src='images/audi.q5.jpg' class='images'>"
+        },
+        Q7: {
+            carName: "Audi Q7",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "3.0L V6",
+            seatCapacity: "7",
+            fuelTankCapacity: "85 l",
+            image: "<img src='images/audi.q7.jpg' class='images'>"
+        },
+        A8: {
+            carName: "Audi A8",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "3.0L V6",
+            seatCapacity: "5",
+            fuelTankCapacity: "82 l",
+            image: "<img src='images/audi.a8.jpg' class='images'>"
+        },
+        TT: {
+            carName: "Audi TT",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.0L I4",
+            seatCapacity: "2+2",
+            fuelTankCapacity: "50 l",
+            image: "<img src='images/audi.t4.jpg' class='images'>"
+        },
+        R8: {
+            carName: "Audi R8",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "5.2L V10",
+            seatCapacity: "2",
+            fuelTankCapacity: "73 l",
+            image: "<img src='images/audi.r8.jpg' class='images'>"
+        },
+        eTron: {
+            carName: "Audi e-Tron",
+            model: "2023",
+            fuelType: "Electric",
+            engine: "Electric",
+            seatCapacity: "5",
+            fuelTankCapacity: "95 kWh",
+            image: "<img src='images/audi.etron.jpg' class='images'>"
+        }
+    },
+    BMW: {
+        ThreeSeries: {
+            carName: "BMW 3 Series",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.0L I4",
+            seatCapacity: "5",
+            fuelTankCapacity: "60 l",
+            image: "<img src='images/bmw.ThreeSeries.jpg' class='images'>"
+        },
+        FiveSeries: {
+            carName: "BMW 5 Series",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.0L I4",
+            seatCapacity: "5",
+            fuelTankCapacity: "68 l",
+            image: "<img src='images/bmw.FiveSeries.jpg' class='images'>"
+        },
+        SevenSeries: {
+            carName: "BMW 7 Series",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "3.0L I6",
+            seatCapacity: "5",
+            fuelTankCapacity: "78 l",
+            image: "<img src='images/bmw.sevenSeries.jpg' class='images'>"
+        },
+        X3: {
+            carName: "BMW X3",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.0L I4",
+            seatCapacity: "5",
+            fuelTankCapacity: "65 l",
+            image: "<img src='images/bmw.x3.jpg' class='images'>"
+        },
+        X5: {
+            carName: "BMW X5",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "3.0L I6",
+            seatCapacity: "5",
+            fuelTankCapacity: "83 l",
+            image: "<img src='images/bmw.x5.jpg' class='images'>"
+        },
+        X7: {
+            carName: "BMW X7",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "3.0L I6",
+            seatCapacity: "7",
+            fuelTankCapacity: "83 l",
+            image: "<img src='images/bmw.x7.jpg' class='images'>"
+        },
+        Z4: {
+            carName: "BMW Z4",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.0L I4",
+            seatCapacity: "2",
+            fuelTankCapacity: "52 l",
+            image: "<img src='images/bmw.z4.jpg' class='images'>"
+        },
+        M3: {
+            carName: "BMW M3",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "3.0L I6",
+            seatCapacity: "5",
+            fuelTankCapacity: "60 l",
+            image: "<img src='images/bmw.m3.jpg' class='images'>"
+        },
+        M5: {
+            carName: "BMW M5",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "4.4L V8",
+            seatCapacity: "5",
+            fuelTankCapacity: "68 l",
+            image: "<img src='images/bmw.m5.jpg' class='images'>"
+        },
+        i4: {
+            carName: "BMW i4",
+            model: "2023",
+            fuelType: "Electric",
+            engine: "Electric",
+            seatCapacity: "5",
+            fuelTankCapacity: "83 kWh",
+            image: "<img src='images/bmw.i4.jpg' class='images'>"
+        }
+    },
+    Ford: {
+        F150: {
+            carName: "Ford F-150",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "3.3L V6",
+            seatCapacity: "5-6",
+            fuelTankCapacity: "136 l",
+            image: "<img src='images/ford.F150.jpg' class='images'>"
+        },
+        Mustang: {
+            carName: "Ford Mustang",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.3L I4 Turbo",
+            seatCapacity: "4",
+            fuelTankCapacity: "60 l",
+            image: "<img src='images/ford.mustang.jpg' alt='Ford F-150' class='images'>"
+        },
+        Escape: {
+            carName: "Ford Escape",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "1.5L I3",
+            seatCapacity: "5",
+            fuelTankCapacity: "55 l",
+            image: "<img src='images/ford.escape.jpg' alt='Ford F-150' class='images'>"
+        },
+        Explorer: {
+            carName: "Ford Explorer",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.3L I4 Turbo",
+            seatCapacity: "7",
+            fuelTankCapacity: "70 l",
+            image: "<img src='images/ford.explorer.jpg' alt='Ford F-150' class='images'>"
+        },
+        Bronco: {
+            carName: "Ford Bronco",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.3L I4 Turbo",
+            seatCapacity: "4-5",
+            fuelTankCapacity: "83 l",
+            image: "<img src='images/ford.bronco.jpg' alt='Ford F-150' class='images'>"
+        },
+        Edge: {
+            carName: "Ford Edge",
+            model: "2023",
+            fuelType: "Petrol",
+            engine: "2.0L I4 Turbo",
+            seatCapacity: "5",
+            fuelTankCapacity: "60 l",
+            image: "<img src='images/ford.edge.jpg' alt='Ford F-150' class='images'>"
+        },
+    }
+}
+
+var company = document.getElementById("company");
+var model = document.getElementById("model");
+var specification = document.getElementById("specification");
+
+for (var key in car) {
+    company.innerHTML += `<option value='${key}'> ${key} </option>`
+}
+
+function selectCompany() {
+
+    model.innerHTML = `<option> Select Model </option>`
+
+    for (var key in car[company.value]) {
+        model.innerHTML += `<option value='${key}'> ${key} </option>`
+    }
+}
+
+function selectModel() {
+
+    var selectCompany = company.value;
+    var selectModel = model.value;
+
+    if (model.value) {
+        var details = car[selectCompany][selectModel]
+
+        specification.innerHTML =
+
+            ` <div class="slide d-flex">
+                <div class="box-1">
+                    <h2 class="carname">${details.carName} <span>(${details.model})</span></h2>
+                    ${details.image}
+                </div>
+                <div class="box-2">
+                    <h2>Details</h2>
+                    <ul>
+                        <li><strong>Model:</strong> ${details.model}</li>
+                        <li><strong>Fuel Type:</strong> ${details.fuelType}</li>
+                        <li><strong>Engine:</strong> ${details.engine}</li>
+                        <li><strong>Seat Capacity:</strong> ${details.seatCapacity}</li>
+                        <li><strong>Fuel Tank Capacity:</strong> ${details.fuelTankCapacity}</li>
+                    </ul>
+                </div>
+            </div>`
+
+
+    }
+    else {
+        specification.innerHTML = "";
+    }
+}
+
+// specification.innerHTML += car.Ford.F150.image;
+// specification.innerHTML += car.Ford.Mustang.image;
+// specification.innerHTML += car.Ford.Escape.image;
+// specification.innerHTML += car.Ford.Explorer.image;
+// specification.innerHTML += car.Ford.Bronco.image;
+// specification.innerHTML += car.Ford.Edge.image;
